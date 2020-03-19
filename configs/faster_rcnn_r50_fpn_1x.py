68b3975c29c0b900bb947bfda96cd3f17101cfbb
Ge Jian <gejian@GedeMacBook-Pro.local>
2020-03-19 23:56:45 +0800

    modify bs&save path

--- configs/faster_rcnn_r50_fpn_1x.py
+++ configs/faster_rcnn_r50_fpn_1x.py
@@ -128,7 +128,7 @@ test_pipeline = [
         ])
 ]
 data = dict(
-    imgs_per_gpu=2,
+    imgs_per_gpu=8,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
@@ -169,7 +169,7 @@ log_config = dict(
 total_epochs = 12
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_r50_fpn_1x'
+work_dir = '/content/drive/''My Drive''/work_dir/faster_rcnn_r50_fpn_1x'
 load_from = None
 resume_from = None
 workflow = [('train', 1)]
